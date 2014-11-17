e4ca406c3a24e79d1f923a599b069d50668c1c83
lemessager <quedupur@hotmail.fr>
2014-11-17 13:27:14 +0100

    lél

--- src/Requettes/TypeDeRequete.java
+++ src/Requettes/TypeDeRequete.java
@@ -4,4 +4,14 @@ public enum TypeDeRequete {
 
     GET,ADD,REMOVE,EDIT,LIST;
 
+    public static TypeDeRequete getRequette(char s){
+        switch (s){
+            case ('G'):return GET;
+            case ('A'):return ADD;
+            case ('R'):return REMOVE;
+            case ('E'):return EDIT;
+            case ('L'):return LIST;
+        }
+        return LIST;
+    }
 }
