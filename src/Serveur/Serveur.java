8aa2362ad03ab7f8e088b588fdf6ed2be3a9af64
rianaR <r.rabehasy@gmail.com>
2014-12-01 16:20:59 +0100

    Threads

--- src/Serveur/Serveur.java
+++ src/Serveur/Serveur.java
@@ -6,15 +6,13 @@ import java.io.*;
 import java.net.*;
 
 
-public class Serveur extends Thread{
+public class Serveur {
 
     public static int port=2000;
 
     private Donnees donnees;
     private Protocole protocole;
-    Reponse reponse;
-    Requete requete;
-    
+
     public Serveur(int port){
         donnees = new Donnees();
         initialiserDonnees();
@@ -36,47 +34,7 @@ public class Serveur extends Thread{
         try {
             while (true) {
                 Socket socket = listener.accept();
-
-                try {
-                    ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-                    ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
-
-
-                    reponse = new Reponse(true, "Bienvenue sur le serveur.",false);
-                    os.writeObject((Object) reponse);
-                    try {
-                        while (true) {
-
-                            if ((requete = (Requete) is.readObject()) != null) {
-                                reponse = protocole.getReponse(requete);
-                                if (reponse != null) {
-                                    os.writeObject((Object) reponse);
-                                    if (reponse.deconnectedFromClient()) {
-                                        throw new IOException();
-                                    }
-                                }
-
-                            }
-                        }
-                    }
-                    catch (IOException e) {
-                        is.close();
-                        os.close();
-                        socket.close();
-                        System.err.println("Deconnexion du client!");
-                    }
-
-                }
-
-                catch (IOException e) {
-                    System.err.println("Erreur de connexion : "+e.getMessage());
-                    System.exit(0);
-                }
-                catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-
+                new Thread(new ThreadClient(socket,protocole)).run();
             }
         }
 
